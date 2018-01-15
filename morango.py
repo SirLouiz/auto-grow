c010bde32ddf8c9e079d41fa0c4df15a1fd4eb6a
Luiz <sirlouiz@gmail.com>
2018-01-15 18:57:59 -0200

    programa do morango

--- morango.py
+++ morango.py
@@ -1,4 +1,6 @@
 import time
+from time import gmtime, strftime
+
 import paho.mqtt.subscribe as subscribe
 
 import RPi.GPIO as GPIO
@@ -13,9 +15,11 @@ GPIO.setmode(GPIO.BCM)
 #removing the warings
 GPIO.setwarnings(False)
 #creating a list (array) with the number of GPIO's that we use
-pins = [13,19,26]
+lights = [13,19]
+bomb = 26
 #setting the mode for all pins so all will be switched on
-GPIO.setup(pins, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(lights, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(bomb, GPIO.OUT, initial=GPIO.HIGH)
 
 # Type of sensor, can be Adafruit_DHT.DHT11, Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 DHT_TYPE = Adafruit_DHT.DHT11
@@ -26,11 +30,20 @@ DHT_PIN  = 4
 SPI_PORT   = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-print("Preparação Concluida")
+print("Preparacao Concluida")
 while True:
-	tempo = strftime(" %H:%M:%S ")
-	if tempo =="17:":
-		print(tempo)
-
-
-
+        soil = mcp.read_adc(0)
+        if soil >= "1000": #if soil is dry
+                GPIO.output(bomb,  GPIO.LOW)
+                continue
+        elif soil <= "600": #if soil is wet
+                GPIO.output(bomb,  GPIO.HIGH)
+	tempo = strftime("%H:%M:%S")
+	tempo = tempo.split(":")
+	if tempo[0] =="18": #if it is 18oclock and the lights are on
+		if GPIO.input(lights)==0:
+			GPIO.output(lights,  GPIO.HIGH) #turn off
+	elif tempo[0] =="06": #if it is 06oclock and the lights are off
+		if GPIO.input(lights)==1:
+			GPIO.output(lights,  GPIO.LOW) #turn on
+	time.sleep(2000) #wait 30min and read again
