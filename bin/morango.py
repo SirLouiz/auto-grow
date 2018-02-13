97a78ffce0362668acdf586c03ec170e3a2399cc
Luiz <sirlouiz@gmail.com>
2018-02-13 14:24:31 -0200

    tempo de irrigação

--- bin/morango.py
+++ bin/morango.py
@@ -33,19 +33,19 @@ SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 #print("Preparacao Concluida")
 while True:
-        soil = mcp.read_adc(0)
-	if soil >= 650: #if soil is dry
-                GPIO.output(pump,  GPIO.LOW) #turn on pump
-                continue
-        elif soil <= 500: #if soil is wet
-                GPIO.output(pump,  GPIO.HIGH) #turn off pump
-	tempo = strftime("%H:%M:%S")
+        tempo = strftime("%H:%M:%S")
         if tempo == "23:59:00":
-		print ("Rebooting")
+		#print ("Rebooting")
 		os.system('systemctl reboot')
 		time.sleep(5)
+        elif tempo == "06:30:00": #waters once a day for 3s
+                GPIO.output(pump, GPIO.LOW) #turn on pump
+                time.sleep(5) #waits 3s
+		GPIO.output(pump, GPIO.HIGH) #turn off pump
+		#time.sleep()
         tempo = tempo.split(":")
-	if tempo[0] =="18" and GPIO.input(13)==0: #if its 18h and lights on
+	if tempo[0]=="17" and tempo[1]=="59" and GPIO.input(13)==0: #if its 18h and lights on
 			GPIO.output(lights,  GPIO.HIGH) #turn off
-	elif tempo[0] =="06" and GPIO.input(13)==1: #if its 06h and lights off
+	elif tempo[0]=="05" and tempo[1]=="59" and GPIO.input(13)==1: #if its 06h and lights off
 			GPIO.output(lights,  GPIO.LOW) #turn on
+        #soil = mcp.read_adc(0)
